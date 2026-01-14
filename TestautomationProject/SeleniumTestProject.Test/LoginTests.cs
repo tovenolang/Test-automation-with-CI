e8b7503131cb021e71d5a0e9bb8732ca357d3a45
Tove Noläng <tove.nolang@strivo.net>
2026-01-14 08:51:13 +0100

    Added Selenium test for invalid credentials

--- TestautomationProject/SeleniumTestProject.Test/LoginTests.cs
+++ TestautomationProject/SeleniumTestProject.Test/LoginTests.cs
@@ -30,6 +30,44 @@ public class LoginTests : IDisposable
         Assert.Contains("inventory", _driver.Url);
     }
 
+    [Fact]
+    public void LoginWithInvalidUsername_Returns_ErrorMessage()
+    {
+        // Arrange
+        var usernameField = _driver.FindElement(By.Id("user-name"));
+        var passwordField = _driver.FindElement(By.Id("password"));
+        var loginButton = _driver.FindElement(By.Id("login-button"));
+
+        usernameField.SendKeys("invalid_username");
+        passwordField.SendKeys("secret_sauce");
+
+        // Act
+        loginButton.Click();
+
+        //Assert
+        var errorMessage = _driver.FindElement(By.ClassName("error-message-container")).Text;
+        Assert.Contains("Epic sadface", errorMessage);
+    }
+
+    [Fact]
+    public void LoginWithInvalidPassword_Returns_ErrorMessage()
+    {
+        // Arrange
+        var usernameField = _driver.FindElement(By.Id("user-name"));
+        var passwordField = _driver.FindElement(By.Id("password"));
+        var loginButton = _driver.FindElement(By.Id("login-button"));
+
+        usernameField.SendKeys("standard_user");
+        passwordField.SendKeys("invalid_password");
+
+        // Act
+        loginButton.Click();
+
+        //Assert
+        var errorMessage = _driver.FindElement(By.ClassName("error-message-container")).Text;
+        Assert.Contains("Epic sadface", errorMessage);
+    }
+
     public void Dispose()
     {
         _driver.Quit();
